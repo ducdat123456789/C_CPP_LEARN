22f3492d95d47211aac24110cd69974b2ffe24c6
= <=>
2022-03-13 14:51:44 +0700

    clear code

--- 1_c/Thang_3_2022/tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
- // if (!(buffer = (char *)malloc(100*sizeof(char))))
-    // {
-    //     printf("loi cap phat!");
-    //     return 1;
-    // }
--- 1_c/Thang_3_2022/test_computer.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    int a=10;
-    printf("\nhello a = %d\n\n", a);
-}
